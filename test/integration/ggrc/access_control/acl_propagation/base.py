11b0b03b054e3271de5cf8417fc73d6e63e94d23
IlyaTsupryk <ilya.tsupryk@gmail.com>
2018-03-02 11:04:02 +0300

    Add clone and read revisions operations

--- test/integration/ggrc/access_control/acl_propagation/base.py
+++ test/integration/ggrc/access_control/acl_propagation/base.py
@@ -14,6 +14,9 @@ class TestACLPropagation(TestCase):
 
   GLOBAL_ROLES = ["Creator", "Reader", "Editor", "Administrator"]
 
+  STATUS_SUCCESS = [200, 201]
+  STATUS_FORBIDDEN = [403,]
+
   def get_user_object(self, role_name):
     return all_models.Person.query.get(self.people_ids[role_name])
 
@@ -34,11 +37,20 @@ class TestACLPropagation(TestCase):
             person=user
         )
 
+  def assert_result(self, responses, expected_res):
+    exp_status = self.STATUS_SUCCESS if expected_res else self.STATUS_FORBIDDEN
+    if not isinstance(responses, list):
+      responses = [responses]
+    for response in responses:
+      self.assertIn(
+          response.status_code,
+          exp_status,
+          "Response for current operation has wrong status.{} expected, "
+          "{} received".format(str(exp_status), response.status_code)
+      )
+
 
 class TestAuditACLPropagation(TestACLPropagation):
-
-  FORBIDDEN_RESPONSE_STATUSES = [403, ]
-  SUCCESSFULL_RESPONSE_STATUSES = [200, 201]
 
   def setup_base_objects(self, global_role):
     raise NotImplementedError()
@@ -84,12 +96,12 @@ class TestAuditACLPropagation(TestACLPropagation):
         }
     })
 
-  def generate_asmnt_without_template(self, model, role):
+  def generate(self, model, role):
     self.setup_base_objects(role)
     snapshot_id = self._create_snapshots(self.audit, [self.control])[0].id
-
     self.api.set_user(self.get_user_object(role))
-    return self.api.post(all_models.Assessment, {
+    responses = []
+    asmnt_data = {
         "assessment": {
             "_generated": True,
             "audit": {
@@ -103,32 +115,15 @@ class TestAuditACLPropagation(TestACLPropagation):
             "context": None,
             "title": "New assessment",
         }
-    })
+    }
+    responses.append(self.api.post(all_models.Assessment, asmnt_data))
 
-  def generate_asmnt_with_template(self, model, role):
-    self.setup_base_objects(role)
-    snapshot_id = self._create_snapshots(self.audit, [self.control])[0].id
-
-    self.api.set_user(self.get_user_object(role))
-    return self.api.post(all_models.Assessment, {
-        "assessment": {
-            "_generated": True,
-            "audit": {
-                "id": self.audit_id,
-                "type": "Audit"
-            },
-            "object": {
-                "id": snapshot_id,
-                "type": "Snapshot"
-            },
-            "context": None,
-            "title": "New assessment",
-            "template": {
-                "id": self.template_id,
-                "type": "AssessmentTemplate"
-            },
-        }
-    })
+    asmnt_data["assessment"]["template"] = {
+        "id": self.template_id,
+        "type": "AssessmentTemplate"
+    }
+    responses.append(self.api.post(all_models.Assessment, asmnt_data))
+    return responses
 
   def create_object(self, model_name):
     method_name = "create_{}".format(model_name.lower())
@@ -138,7 +133,7 @@ class TestAuditACLPropagation(TestACLPropagation):
                       format(model_name))
 
     response = bound_method(model_name)
-    if response.status_code in self.FORBIDDEN_RESPONSE_STATUSES:
+    if response.status_code in self.STATUS_FORBIDDEN:
       raise Exception("Can't create '{}' object. '{}' error".
                       format(model_name, response.status))
     return response.json.get(model_name.lower(), {}).get("id")
@@ -147,7 +142,7 @@ class TestAuditACLPropagation(TestACLPropagation):
     """Test access to model endpoint."""
     self.api.set_user(self.get_user_object(role))
     model_class = get_model(model)
-    # ToDo: Check reading single object also
+    # ToDo: Check reading of single object also
     return self.api.get_query(model_class, "")
 
   def update(self, model, role):
@@ -165,7 +160,34 @@ class TestAuditACLPropagation(TestACLPropagation):
     return self.api.delete(obj)
 
   def clone(self, model, role):
-    return None
+    self.setup_base_objects(role)
+    self.api.set_user(self.get_user_object(role))
+    return self.api.post(all_models.Audit, {
+        "audit": {
+            "program": {"id": self.program_id},
+            "context": None,
+            "operation": "clone",
+            "cloneOptions": {
+                "sourceObjectId": self.audit_id,
+                "mappedObjects": "AssessmentTemplate"
+            }
+        }
+    })
+
+  def read_revisions(self, model, role):
+    self.setup_base_objects(role)
+    self.api.set_user(self.get_user_object(role))
+    model_class = get_model(model)
+    responses = []
+    for query in ["source_type={}&source_id={}",
+                  "destination_type={}&destination_id={}",
+                  "resource_type={}&resource_id={}"]:
+      id_name = "{}_id".format(model.lower())
+      id = getattr(self, id_name)
+      responses.append(
+          self.api.get_query(model_class, query.format(model, id))
+      )
+    return responses
 
   def runtest(self, role, model, action_str, expected_result):
     action = getattr(self, action_str, None)
@@ -173,7 +195,4 @@ class TestAuditACLPropagation(TestACLPropagation):
       raise NotImplementedError(
         "Action {} is not implemented for this test.".format(action_str)
       )
-    response = action(model, role)
-    expected_statuses = self.SUCCESSFULL_RESPONSE_STATUSES if expected_result \
-      else self.FORBIDDEN_RESPONSE_STATUSES
-    self.assertEqual(response.status_code in expected_statuses, True)
+    self.assert_result(action(model, role), expected_result)
