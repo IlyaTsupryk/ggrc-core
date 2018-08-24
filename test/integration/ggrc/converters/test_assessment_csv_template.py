782ffc137faae52024a1edd541f9f0fa96788a9a
IlyaTsupryk <ilya.tsupryk@gmail.com>
2018-08-24 12:33:54 +0300

    Test local cads in assessment csv template

--- test/integration/ggrc/converters/test_assessment_csv_template.py
+++ test/integration/ggrc/converters/test_assessment_csv_template.py
@@ -40,3 +40,32 @@ class TestAssessmentCSVTemplate(TestCase):
     response = self.export_csv(data)
     self.assertEqual(response.status_code, 200)
     self.assertIn("Verifiers,Comments,Last Comment,GCA 1", response.data)
+
+  def test_local_cads_abset(self):
+    """Test if local CADs absent in Assessment csv template."""
+    with factories.single_commit():
+      asmnt = factories.AssessmentFactory()
+      factories.CustomAttributeDefinitionFactory(
+          definition_type="assessment",
+          definition_id=asmnt.id,
+          title="LCA 1",
+      )
+      asmnt_template = factories.AssessmentTemplateFactory()
+      factories.CustomAttributeDefinitionFactory(
+          definition_type="assessment_template",
+          definition_id=asmnt_template.id,
+          title="LCA 2",
+      )
+      factories.CustomAttributeDefinitionFactory(
+          definition_type="assessment", title="GCA 1",
+      )
+
+    data = [{
+        "object_name": "Assessment",
+        "fields": "all",
+    }]
+    response = self.export_csv(data)
+
+    self.assertEqual(response.status_code, 200)
+    self.assertNotIn("LCA 1", response.data)
+    self.assertNotIn("LCA 2", response.data)
